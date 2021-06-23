5c156253c5e7ba445ea48d5b0c82878061972d7c
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-06-23 10:26:17 -0300

    adaptação do preload para eventos de requizição de nova janela

--- electron/preload.js
+++ electron/preload.js
@@ -1,3 +1,20 @@
+const { ipcRenderer } = require('electron')
 document.addEventListener("DOMContentLoaded", ()=> {
+    const isMain = location.href.search('index.html') !== -1
+
+    if(isMain){
+        const imageLinks = document.querySelectorAll('ul > li > img')
+        imageLinks.forEach( i => {
+            i.onclick = event =>{
+                const href = event.target.dataset.href
+
+                ipcRenderer.send('new-window',href)
+
+            }
+        })
+    }else{
+        document.body.style.padding = "5px"
+    }
+
     require("./header-actions-renderer.js")
 })
