6880e58b799a0aceaec92cb2e57a69288c1232ba
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-06-23 10:24:53 -0300

    Função de criar nova janela

--- electron/main.js
+++ electron/main.js
@@ -23,6 +23,25 @@ function mainWindow(){
     win.loadFile("index.html")
 }
 
+function createWindow(href){
+    const win = new BrowserWindow({
+        frame: false,
+        width: 315,
+        minWidth: 315,
+        height: 456,
+        minHeight: 456,
+        resizable: false,
+        maximizable: false,
+        icon: path.join(__dirname,"../assets/icon.png"),
+        webPreferences:{
+            nodeIntegration: true,
+            preload: path.join(__dirname, "preload.js")
+        }
+    })
+    win.setMenuBarVisibility(false)
+    win.loadFile(path.join(__dirname ,'../', href))
+}
+
 app.whenReady().then(mainWindow)
 
 app.on('window-all-closed', () => {
