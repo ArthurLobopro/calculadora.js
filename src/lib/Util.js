2b8986b1a5cb4db61eab0905b50d569cd6ddfd29
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2022-03-03 11:39:31 -0300

    feat: add range function in Util module

--- src/lib/Util.js
+++ src/lib/Util.js
@@ -5,4 +5,24 @@ function loadSVG(...PathSegments) {
     return fs.readFileSync(path.resolve(...PathSegments), {encoding: "utf-8"})
 }
 
-module.exports =  { loadSVG }
+const range = (min,max,pass=1) => {
+    if(pass == 0){
+        throw new Error("Pass can't be equal to 0")
+    }
+
+    let array = []
+    if(min > max){
+        pass = pass > 0 ? pass : pass * -1
+        for(let i = min;i>=max;i-=pass){ array.push(i) }
+        return array
+    }
+
+    if(pass < 0){
+        throw new Error("Pass can't be less than 0 on crescent range")
+    }
+
+    for(let i = min;i<=max;i+=pass){ array.push(i) }
+    return array
+}
+
+module.exports =  { loadSVG, range }
