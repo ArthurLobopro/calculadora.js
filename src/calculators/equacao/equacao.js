a2ebc3000629a3ee80f7d165663498c1722de42e
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2022-03-02 22:56:45 -0300

    feat: add arrow up and  arrow down functions

--- src/calculators/equacao/equacao.js
+++ src/calculators/equacao/equacao.js
@@ -8,52 +8,58 @@ const circle = loadSVG(appPath, "assets", "circle-x.svg")
 const a_input = document.getElementById("A")
 const b_input = document.getElementById("B")
 const c_input = document.getElementById("C")
+const inputs = [a_input, b_input, c_input]
 
 const validadeKey = e => {
-    return e.key == "Backspace" || "-0123456789".indexOf(e.key) >= 0 || e.key == ',' && !e.target.value.includes(',')
+    return (
+        e.key === "Backspace" || Array.from("0123456789").includes(e.key) ||
+        e.key === ',' && !e.target.value.includes(',') &&
+        !['+', '-'].includes(e.key)
+    )
+}
+
+const negativeAndPositiveValidate = event => {
+    const value = event.currentTarget.value
+
+    if (event.key === "-" && !value.includes('-') || event.key === "+" && Number(value) < 0) {
+        event.target.value = Number(value) * -1
+    }
 }
 
 const arrows = event => {
-    const elements = [a_input, b_input, c_input]
-    const target = event.target
-    const index = elements.indexOf(target)
-    const key = event.key
+    const { target, key } = event
+    const index = inputs.indexOf(target)
+    const value = Number(target.value)
 
-    if (index != elements.length - 1 && ["ArrowRight", "Enter"].includes(key)) {
-        elements[index + 1].focus()
+    if (["ArrowUp", "ArrowDown"].includes(key)) {
+        target.value = key === "ArrowUp" ? value + 1 : value - 1
     }
 
-    if(index === elements.length - 1 && key === "Enter"){
-        calc()
+    if (index != inputs.length - 1 && ["ArrowRight", "Enter"].includes(key)) {
+        inputs[index + 1].focus()
+    }
+
+    if (index === inputs.length - 1 && key === "Enter") {
+        calcAndAppendResult()
     }
 
     if (index > 0 && key === "ArrowLeft") {
-        elements[index - 1].focus()
+        inputs[index - 1].focus()
     }
 }
 
-const onkeydown = event => { arrows(event); validadeKey(event) }
+const onkeydown = event => {
+    arrows(event);
+    negativeAndPositiveValidate(event)
+    return validadeKey(event);
+}
 
-[a_input, b_input, c_input].forEach(input => input.onkeydown = onkeydown)
+inputs.forEach(input => input.onkeydown = onkeydown)
 
-function buildRes({ a, b, c, x = [], delta, root }) {
+function buildRes(content) {
     const res = document.createElement("div")
     res.className = "res"
-
-    const xString = String(root).toLowerCase() == "inexistente" ? "Inexistente" :
-        x.length == 1 ? x[0] : `${x[0]} ou ${x[1]}`
-
-    res.innerHTML = `
-        <div>
-            A: ${a} <br>
-            B: ${b} <br>
-            C: ${c} <br>
-            Δ: ${delta} <br>
-            Raiz: ${root} <br>
-            X: ${xString}
-        </div>
-        ${circle}
-    `
+    res.innerHTML = `${content}${circle}`
 
     res.querySelector('svg').onclick = () => {
         resultDiv.removeChild(res)
@@ -71,32 +77,37 @@ function getInputValues() {
 }
 
 function clearInputValues() {
-    a_input.value = ''
-    b_input.value = ''
-    c_input.value = ''
+    inputs.forEach(input => input.value = '')
 }
 
-function calc() {
+function calcAndAppendResult() {
     const { a, b, c } = getInputValues()
     if (a === 0) {
         a_input.style.borderBottomColor = "red"
         a_input.focus()
     } else {
-        let delta = (b * b) - 4 * a * c
-        let raiz = Math.sqrt(delta)
+        const delta = (b * b) - 4 * a * c
+        const root = Math.sqrt(delta)
         let x1, x2
 
-        x1 = (-(b) - raiz) / (a * 2)
-        x2 = (-(b) + raiz) / (a * 2)
+        x1 = (-(b) - root) / (a * 2)
+        x2 = (-(b) + root) / (a * 2)
 
-        resultDiv.appendChild(
-            buildRes({
-                a, b, c, delta,
-                x: x1 == x2 ? [x1] : [x1, x2],
-                root: raiz >= 0 ? raiz : "Inexistente"
-            })
-        )
+        const x = x1 == x2 ? [x1] : [x1, x2]
+        const xString = String(root).toLowerCase() == "inexistente" ? "Inexistente" :
+            x.length == 1 ? x[0] : `${x[0]} ou ${x[1]}`
 
+        const content = `
+        <div>
+            A: ${a} <br>
+            B: ${b} <br>
+            C: ${c} <br>
+            Δ: ${delta} <br>
+            Raiz: ${root >= 0 ? root : "Inexistente"} <br>
+            X: ${xString}
+        </div>`
+
+        resultDiv.appendChild(buildRes(content))
         clearInputValues()
         a_input.focus()
     }
@@ -106,4 +117,4 @@ a_input.addEventListener('keyup', () => {
     a_input.style.borderBottomColor = a_input.value == "0" ? "red" : ""
 })
 
-document.querySelector("#calc").onclick = calc
+document.querySelector("#calc").onclick = calcAndAppendResult
