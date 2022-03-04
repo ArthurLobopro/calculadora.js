d792aa1d24ab90aa5d6308a5172f19c54ce26dc0
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2022-03-04 15:36:21 -0300

    refactor: remove lib from dependencies

--- update-lib.js
+++ update-lib.js
@@ -3,10 +3,7 @@ const path = require("path");
 
 console.log("===Instalando dependências===");
 
-const packages = [
-    "https://github.com/ArthurLobopro/time-converter",
-    "./src/lib"
-]
+const packages = ["https://github.com/ArthurLobopro/time-converter"]
 
 exec(`yarn add ${packages.join(' ')} --cwd ${__dirname}`, (error, stdout, stderr) => {
     if (error) {
