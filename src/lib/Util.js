a818877ec0c3d8a56517ca482573ce875fdac62d
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2022-03-04 12:52:23 -0300

    refactor: upgrade createElement function

--- src/lib/Util.js
+++ src/lib/Util.js
@@ -2,37 +2,37 @@ const path = require('path')
 const fs = require('fs')
 
 function loadSVG(...PathSegments) {
-    return fs.readFileSync(path.resolve(...PathSegments), {encoding: "utf-8"})
+    return fs.readFileSync(path.resolve(...PathSegments), { encoding: "utf-8" })
 }
 
-const range = (min,max,pass=1) => {
-    if(pass == 0){
+const range = (min, max, pass = 1) => {
+    if (pass == 0) {
         throw new Error("Pass can't be equal to 0")
     }
 
     let array = []
-    if(min > max){
+    if (min > max) {
         pass = pass > 0 ? pass : pass * -1
-        for(let i = min;i>=max;i-=pass){ array.push(i) }
+        for (let i = min; i >= max; i -= pass) { array.push(i) }
         return array
     }
 
-    if(pass < 0){
+    if (pass < 0) {
         throw new Error("Pass can't be less than 0 on crescent range")
     }
 
-    for(let i = min;i<=max;i+=pass){ array.push(i) }
+    for (let i = min; i <= max; i += pass) { array.push(i) }
     return array
 }
 
-const createElement = (elementName = 'div', options = {}) => {
+const createElement = (elementName = 'div', {dataset = {}, ...options}) => {
     const element = document.createElement(elementName)
 
-    for (const optionName in options) {
-        element[optionName] = options[optionName]
-    }
+    Object.entries(dataset).forEach( ([property, value]) => element.dataset[property] = value)
+
+    Object.entries(options).forEach(([property, value]) => element[property] = value)
 
     return element
 }
 
-module.exports =  { loadSVG, range, createElement }
+module.exports = { loadSVG, range, createElement}
