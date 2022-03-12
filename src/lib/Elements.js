c60b7b2203279c7334468fbe33119d1c49fc8302
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2022-03-12 09:09:28 -0300

    refactor: create Elements module

---
+++ src/lib/Elements.js
@@ -0,0 +1,53 @@
+const { loadSVG } = require("./Util")
+const circle = loadSVG(appPath, "assets", "circle-x.svg")
+
+function buildResultDiv(content, resultWrapper) {
+    const result_div = document.createElement("div")
+    result_div.className = "res"
+    result_div.innerHTML = `${content}${circle}`
+
+    result_div.querySelector('svg').onclick = () => {
+        resultWrapper.removeChild(result_div)
+    }
+
+    resultWrapper.appendChild(result_div)
+}
+
+function createElement(elementName = 'div', { dataset = {}, ...options }) {
+    const element = document.createElement(elementName)
+
+    Object.entries(dataset).forEach(([property, value]) => element.dataset[property] = value)
+
+    Object.entries(options).forEach(([property, value]) => element[property] = value)
+
+    return element
+}
+
+function createButtonList(buttonListOptions = [{ dataset, content, ...options }]) {
+    return buttonListOptions.map(({ dataset, content, ...options }) => {
+        return createElement('button', { dataset, innerHTML: content, ...options }).outerHTML
+    })
+}
+
+class CalculatorBase {
+    constructor() {
+        this.build()
+        this?.addEvents()
+    }
+
+    appendMetadata() {
+        this.getMetadata().forEach(tag => document.head.appendChild(tag))
+    }
+
+    append() {
+        this.appendMetadata()
+        document.body.appendChild(this.screen)
+    }
+}
+
+module.exports = Object.freeze({
+    buildResultDiv,
+    CalculatorBase,
+    createButtonList,
+    createElement
+})
