2a6dd60f713402efcd6e4cd2e32c8ca677c2ae21
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2022-03-12 09:10:28 -0300

    refactor: remove elements functions to Util module

--- src/lib/Util.js
+++ src/lib/Util.js
@@ -25,20 +25,6 @@ const range = (min, max, pass = 1) => {
     return array
 }
 
-const createElement = (elementName = 'div', {dataset = {}, ...options}) => {
-    const element = document.createElement(elementName)
 
-    Object.entries(dataset).forEach( ([property, value]) => element.dataset[property] = value)
 
-    Object.entries(options).forEach(([property, value]) => element[property] = value)
-
-    return element
-}
-
-const createButtonList = (buttonListOptions = [{dataset, content, ...options}]) => {
-    return buttonListOptions.map( ({dataset, content, ...options}) => {
-        return createElement('button', {dataset, innerHTML: content, ...options}).outerHTML
-    })
-}
-
-module.exports = { loadSVG, range, createElement, createButtonList}
+module.exports = { loadSVG, range }
