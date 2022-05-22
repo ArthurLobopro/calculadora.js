421c8589ec98befd8364e181253d747ce36e7b68
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2022-05-22 16:17:38 -0300

    chore: update electron frame

--- src/main/preload.js
+++ src/main/preload.js
@@ -1,8 +1,8 @@
 const { ipcRenderer, contextBridge } = require('electron')
-const { insertFrame } = require('electron-frame/renderer')
+const { electronFrame } = require('electron-frame/renderer')
 
 const getArgValue = arg => {
-    const argString = process.argv.find( argvArg => argvArg.includes(arg))
+    const argString = process.argv.find(argvArg => argvArg.includes(arg))
     return argString.split("=")[1]
 }
 
@@ -10,24 +10,25 @@ contextBridge.exposeInMainWorld('require', require)
 contextBridge.exposeInMainWorld('appPath', getArgValue("--app-path"))
 contextBridge.exposeInMainWorld('ipcRenderer', ipcRenderer)
 
-document.addEventListener("DOMContentLoaded", ()=> {
+document.addEventListener("DOMContentLoaded", () => {
     const isMain = location.href.search('index.html') !== -1
 
-    if(isMain){
+    if (isMain) {
         const imageLinks = document.querySelectorAll('ul > li > img')
-        imageLinks.forEach( i => {
-            i.onclick = event =>{
+        imageLinks.forEach(i => {
+            i.onclick = event => {
                 const href = event.target.dataset.href
 
-                ipcRenderer.send('new-window',href)
+                ipcRenderer.send('new-window', href)
 
             }
         })
-    }else if(process.isMainFrame){
+    } else if (process.isMainFrame) {
         document.body.style.padding = "5px"
     }
 
-    if(process.isMainFrame){
-        insertFrame()
-    }    
+    if (process.isMainFrame) {
+        const frame = new electronFrame()
+        frame.insert()
+    }
 })
