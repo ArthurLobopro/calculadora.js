9dcb3a15a9bb62d5f997385655d9b7701ddaaf9b
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2023-01-07 17:06:22 -0300

    refactor: fix bases calculator layout

---
+++ src/calculators/bases/buttonsLayout.tsx
@@ -0,0 +1,23 @@
+import { assetsPath } from "../../Util"
+import { resolve } from "path"
+
+export const buttons = [
+    { value: "Delete", content: "CE" },
+    { value: "Backspace", content: <img src={resolve(assetsPath, "keyboard/delete.svg")} alt="Apagar" /> },
+    { value: "0", content: "0" },
+    { value: "A", content: "A" },
+    { value: "B", content: "B" },
+    { value: "C", content: "C" },
+    { value: "1", content: "1" },
+    { value: "2", content: "2" },
+    { value: "3", content: "3" },
+    { value: "D", content: "D" },
+    { value: "4", content: "4" },
+    { value: "5", content: "5" },
+    { value: "6", content: "6" },
+    { value: "E", content: "E" },
+    { value: "7", content: "7" },
+    { value: "8", content: "8" },
+    { value: "9", content: "9" },
+    { value: "F", content: "F" }
+]
--- src/renderer/renderer.tsx
+++ src/renderer/renderer.tsx
@@ -1,27 +1,3 @@
-// import { ipcRenderer } from "electron"
-
-// const iframe = document.querySelector('iframe') as HTMLIFrameElement
-// const nameDiv = document.getElementById('calculator-name') as HTMLDivElement
-
-// const menuExpand = document.querySelector('#container #menu-expand') as HTMLDivElement
-
-// const buttons = document.querySelectorAll('ul > li') as NodeListOf<HTMLLIElement>
-// buttons.forEach(e => {
-//     e.onclick = (event: MouseEvent) => {
-//         const target = event.currentTarget as HTMLElement
-//         if (target.tagName === 'IMG') return
-//         const src = target.dataset.src as string
-
-//         if (iframe.src !== src) iframe.src = src
-
-//         menuExpand.click()
-//         iframe.focus()
-
-//         nameDiv.innerText = String(target.textContent).trim()
-//     }
-// })
-
-// toggleAlwaysOnTop_button.onclick = toggleAlwaysOnTop
 
 // window.onresize = () => {
 //     const container = document.getElementById('container') as HTMLDivElement
@@ -34,7 +10,7 @@ import ReactDOM from "react-dom/client"
 import { App } from "./App"
 
 const root = ReactDOM.createRoot(
-    document.getElementById("container") as HTMLElement
+    document.getElementById("root") as HTMLElement
 )
 
 root.render(
