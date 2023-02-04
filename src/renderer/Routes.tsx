0af1d9032c6cd3447be106e228962122fb1c128e
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2023-02-04 12:53:37 -0300

    chore: build fixes

--- src/renderer/Routes.tsx
+++ src/renderer/Routes.tsx
@@ -1,11 +1,12 @@
-import { Route, Routes, HashRouter as Router } from "react-router-dom"
+import { Route, HashRouter as Router, Routes } from "react-router-dom"
 import { Home } from "./pages/Home"
-import { PACalculator } from "./pages/pa"
 import { BasesCalculator } from "./pages/bases"
-import { PGCalculator } from "./pages/pg"
 import { DataCalculator } from "./pages/data"
 import { EquationCalculator } from "./pages/equacao"
+import { PACalculator } from "./pages/pa"
 import { DefaultCalculator } from "./pages/padrao"
+import { PGCalculator } from "./pages/pg"
+import { TimeCalculator } from "./pages/time"
 
 export function AppRoutes() {
     return (
@@ -16,6 +17,7 @@ export function AppRoutes() {
                 <Route path="/bases" element={<BasesCalculator />} />
                 <Route path="/pg" element={<PGCalculator />} />
                 <Route path="/pa" element={<PACalculator />} />
+                <Route path="/time" element={<TimeCalculator />} />
                 <Route path="/data" element={<DataCalculator />} />
                 <Route path="/equacao" element={<EquationCalculator />} />
             </Routes>
